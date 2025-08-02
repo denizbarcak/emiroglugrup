e7f5a58413be7d3b7ac53e82d2bf86d9d4e110c8
dnzbrck <denizbarcakk@gmail.com>
2025-08-02 14:08:22 +0300

    Add complete project with API and UI codee

--- emiroglu-ui/next.config.mjs
+++ emiroglu-ui/next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:8080",
-    NEXT_PUBLIC_ASSETS_URL: "http://localhost:8080",
+    NEXT_PUBLIC_API_URL: "http://135.181.249.171:8080",
+    NEXT_PUBLIC_ASSETS_URL: "http://135.181.249.171:8080",
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: "http://135.181.249.171:8080/api/:path*",
       },
       {
         source: "/assets/:path*",
-        destination: "http://localhost:8080/assets/:path*",
+        destination: "http://135.181.249.171:8080/assets/:path*",
       },
     ];
   },
